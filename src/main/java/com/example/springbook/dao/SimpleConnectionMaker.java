caecc28aca4109b6341ab7e0f13ad6b5b5c26f2d
Reddyong <d0209hong@naver.com>
2024-10-17 21:03:19 +0900

    feat : Connection을 가져오는 기능을 클래스의 분리를 통해 확장하였다.

---
+++ src/main/java/com/example/springbook/dao/SimpleConnectionMaker.java
@@ -0,0 +1,16 @@
+package com.example.springbook.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class SimpleConnectionMaker {
+    public Connection makeNewConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection c = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/mydb", "root", "root1234!!"
+        );
+
+        return c;
+    }
+}
